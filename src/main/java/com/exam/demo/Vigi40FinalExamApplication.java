62451089303b708ee087df9264d45496e6138bb7
tastymoistwindow <ke.luksas@gmail.com>
2023-03-20 19:12:48 +0200

    Spring template loaded

---
+++ src/main/java/com/exam/demo/Vigi40FinalExamApplication.java
@@ -0,0 +1,13 @@
+package com.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vigi40FinalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vigi40FinalExamApplication.class, args);
+	}
+
+}
